fb44ef0dae5b5f1720bb04c64740acb626200c42
John Chang <johnchang9094@gmail.com>
2017-03-03 11:23:57 +0000

    Implementing Hit POints

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,16 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :hit_points
 
-  def initialize(name)
+  HIT_POINTS = 100
+
+  def initialize(name, hit_points = HIT_POINTS)
     @name = name
+    @hit_points = hit_points
+  end
+
+  def reduce_hp
+    @hit_points -= 10
   end
 
 end
