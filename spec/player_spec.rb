b9d35ee1bec2da6f814d4f0b5720a57501c57b48
John Chang <johnchang9094@gmail.com>
2017-03-03 12:05:33 +0000

    Added tests for implementing HP and Player Class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,32 @@
 require 'player'
 
-describe Player do
-  subject(:player) { described_class.new ("John")}
+  describe Player do
+    subject(:john) { described_class.new ("John")}
+    subject(:flora) { described_class.new ("Flora")}
 
-
-  it "expects to return the player's name" do
-    expect(player.name).to eq("John")
+    describe '#name' do
+    it "expects to return the player's name" do
+      expect(john.name).to eq("John")
+    end
   end
+
+  describe '#hit points' do
+    it 'returns the hitpoints' do
+      expect(john.hit_points).to eq(100)
+    end
+  end
+
+  describe '#attack' do
+    it 'attacks the other player' do
+      expect(flora).to receive(:reduce_hp)
+      john.attack(flora)
+    end
+  end
+
+    describe '#reduce_hp' do
+      it 'reduces its own hitpoints by 10' do
+        expect{flora.reduce_hp}.to change {flora.hit_points}.by(-10)
+    end
+    end
+
 end
